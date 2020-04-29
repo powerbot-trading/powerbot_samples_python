5a9145c6a731d16c47fa1b66adbecb078b20d0cf
alexander.benkoe <alex.benkoe@hotmail.com>
2020-04-29 16:03:23 +0200

    Bugfix in simple rest example

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -2,6 +2,6 @@ from pathlib import Path
 import yaml
 
 curr_path = str(Path.cwd()).split("\\")
-config_path = ("\\").join(curr_path[:curr_path.index('powerbot_samples') + 1]) + "/configuration/config.yaml"
+config_path = ("\\").join(curr_path[:curr_path.index('powerbot_samples_python') + 1]) + "/configuration/config.yaml"
 with open(config_path, "r") as configfile:
     config = yaml.full_load(configfile)
